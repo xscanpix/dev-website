586e56929ccbb6f30ec635973697422bb941d47f
Svante Nilsson <svante.n@live.se>
2024-09-27 10:56:15 -0600

    Fixes to support shadcn

--- packages/website/eslint.config.mjs
+++ packages/website/eslint.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
 
+import globals from "globals";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
 );
